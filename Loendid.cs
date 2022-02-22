5ed9eab839e76517e9cf37cc073b4f0651ced558
Эдгар Неверовский <edgarneverovskij@gmail.com>
2022-02-22 21:41:59 +0200

    +

--- Loendid.cs
+++ Loendid.cs
@@ -124,71 +124,93 @@ namespace Console_App1
 
 
             //3 Ülesanne
-            List<string> tooded = new List<string>();
-            tooded.Add("leib");
-            tooded.Add("piim");
-            tooded.Add("õun");
-            List<double> kalorid = new List<double>();
-            kalorid.Add(265);
-            kalorid.Add(42);
-            kalorid.Add(52);
-            Console.WriteLine("Olete naine või mees? (naine/mees)");
-            string soo = Console.ReadLine();
-            Console.WriteLine("Palun sisetage oma kaal, pikkus ja vanus");
-            int kaal = int.Parse(Console.ReadLine());
-            double pikkus = double.Parse(Console.ReadLine());
-            int vanus = int.Parse(Console.ReadLine());
-            Console.WriteLine("Passiivne elustiil/1, Vähe tegevust/2, Mõõdukas aktiivsus/3, Kõrge aktiivsus/4, Väga kõrge aktiivsus/5");
-            string sport = Console.ReadLine();
-            double BOO = 0;
-            double calories = 0;
-            if (soo == "mees")
-            {
-                BOO = 66 + (13.7 * kaal) + (5 * pikkus) - (6.8 * vanus);
-            }
-            else if (soo == "naine")
-            {
-                BOO = 655 + (9.6 * kaal) + (1.8 * pikkus) - (4.7 * vanus);
-            }
-            if (sport == "1")
-            {
-                calories = BOO * 1.2;
-            }
-            else if (sport == "2")
-            {
-                calories = BOO * 1.375;
-            }
-            else if (sport == "3")
-            {
-                calories = BOO * 1.55;
-            }
-            else if (sport == "4")
-            {
-                calories = BOO * 1.725;
-            }
-            else if (sport == "5")
-            {
-                calories = BOO * 1.9;
-            }
-            Console.WriteLine($"Teie kalorite norm päevas - {Math.Round(calories, 2)}");
-            foreach (var item in tooded)
-            {
-                Console.Write(item + ", ");
-            }
-            Console.WriteLine();
-            foreach (int toode in kalorid)
-            {
-                Console.WriteLine(Math.Round(calories / toode, 2));
-            }
+            //List<string> tooded = new List<string>();
+            //tooded.Add("leib");
+            //tooded.Add("piim");
+            //tooded.Add("õun");
+            //List<double> kalorid = new List<double>();
+            //kalorid.Add(265);
+            //kalorid.Add(42);
+            //kalorid.Add(52);
+            //Console.WriteLine("Olete naine või mees? (naine/mees)");
+            //string soo = Console.ReadLine();
+            //Console.WriteLine("Palun sisetage oma kaal, pikkus ja vanus");
+            //int kaal = int.Parse(Console.ReadLine());
+            //double pikkus = double.Parse(Console.ReadLine());
+            //int vanus = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Passiivne elustiil/1, Vähe tegevust/2, Mõõdukas aktiivsus/3, Kõrge aktiivsus/4, Väga kõrge aktiivsus/5");
+            //string sport = Console.ReadLine();
+            //double BOO = 0;
+            //double calories = 0;
+            //if (soo == "mees")
+            //{
+            //    BOO = 66 + (13.7 * kaal) + (5 * pikkus) - (6.8 * vanus);
+            //}
+            //else if (soo == "naine")
+            //{
+            //    BOO = 655 + (9.6 * kaal) + (1.8 * pikkus) - (4.7 * vanus);
+            //}
+            //if (sport == "1")
+            //{
+            //    calories = BOO * 1.2;
+            //}
+            //else if (sport == "2")
+            //{
+            //    calories = BOO * 1.375;
+            //}
+            //else if (sport == "3")
+            //{
+            //    calories = BOO * 1.55;
+            //}
+            //else if (sport == "4")
+            //{
+            //    calories = BOO * 1.725;
+            //}
+            //else if (sport == "5")
+            //{
+            //    calories = BOO * 1.9;
+            //}
+            //Console.WriteLine($"Teie kalorite norm päevas - {Math.Round(calories, 2)}");
+            //foreach (var item in tooded)
+            //{
+            //    Console.Write(item + ", ");
+            //}
+            //Console.WriteLine();
+            //foreach (int toode in kalorid)
+            //{
+            //    Console.WriteLine(Math.Round(calories / toode, 2));
+            //}
 
             //4 Ülesanne
+            //Console.WriteLine("1. Kõik riigid ja pealinnad \n2. Otsimine \n3. Uus sõna \n4. Test");
+            //int vastus = int.Parse(Console.ReadLine());
+            //Dictionary<string, string> countries = new Dictionary<string, string>
+            //{
+            //    {"Prantsusmaa", "Pariis"},
+            //    {"Saksamaa", "Berlin"},
+            //    {"Suur-Britannia", "London"}
+            //};
+            //if (vastus == 1)
+            //{
+            //    foreach (var pair in countries)
+            //    {
+            //        Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            //    }
+            //}
+            //else if (vastus == 2)
+            //{
+            //    string slovo = Console.ReadLine();
+            //    string otsing = countries[slovo];
+            //    Console.WriteLine(otsing);
+            //}
+            //else if (vastus == 3)
+            //{
 
+            //}
+            //else if (vastus == 4)
+            //{
 
-
-
-
-
-
+            //}
 
             Console.ReadLine();
         }
